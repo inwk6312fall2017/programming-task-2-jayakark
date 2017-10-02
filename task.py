134e59d891fe25a85e388b18b28c96af6a8ef647
jayakark <jayakar.k@dal.ca>
2017-10-02 12:34:04 -0300

    commit

--- task.py
+++ task.py
@@ -1,6 +1,7 @@
 import csv
 with open('Crime.csv') as myfile:
 
-reader=csv.reader(myfile)
-for row in reader:
- print(row)
+ reader=csv.reader(myfile)
+ for row in reader:
+  a=list(row)
+  print(a)
