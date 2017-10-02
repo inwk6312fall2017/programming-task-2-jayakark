5c1ce00636039fa7c8d5eb79adcee25a1fd79bed
jayakark <jayakar.k@dal.ca>
2017-10-02 12:50:31 -0300

    commit

--- task.py
+++ task.py
@@ -2,6 +2,11 @@ import csv
 with open('Crime.csv') as myfile:
 
  reader=csv.reader(myfile)
+ a=[]
+ b=[] 
  for row in reader:
   a=list(row)
-  print(a)
+  a.append(a)
+
+  b.append(a[::1])
+  print(b)
